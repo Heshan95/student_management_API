aad5a1151aa4931a73e0440ee4439a27c3456608
Heshan95 <heshankulasooriya.info@gmai.com>
2024-03-25 12:11:38 +0530

    Implementing employeeAPI.java

--- src/main/java/com/example/demo/api/EmployeeAPI.java
+++ src/main/java/com/example/demo/api/EmployeeAPI.java
@@ -1,29 +1,37 @@
 package com.example.demo.api;
 
 import com.example.demo.dto.EmployeeDTO;
+import com.example.demo.service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeAPI {
 
+    private final EmployeeService service;
+
+    public EmployeeAPI(EmployeeService service) {
+        this.service = service;
+    }
+
     @PostMapping(value = "/save")
     public void save(@RequestBody EmployeeDTO obj) {
-        System.out.println(obj);
+        service.save(obj);
     }
 
     @PutMapping(value = "/update")
-    public void update(@RequestBody EmployeeDTO obj){
-        System.out.println(obj);
+    public void update(@RequestBody EmployeeDTO obj) {
+        service.update(obj);
     }
 
     @GetMapping(value = "/get/{id:^EMP_[\\d]{3}$}")
-    public EmployeeDTO search(@PathVariable String id){
-        return new EmployeeDTO("EMP_001", "EmployeeDTO one", 35, "employee@gmail.com");
+    public EmployeeDTO search(@PathVariable String id) {
+        EmployeeDTO search = service.search(id);
+        return search;
     }
 
     @DeleteMapping(value = "/delete/{id:^EMP_[\\d]{3}$}")
-    public void delete(@PathVariable String id){
-        System.out.println(id);
+    public void delete(@PathVariable String id) {
+        service.delete(id);
     }
 }
